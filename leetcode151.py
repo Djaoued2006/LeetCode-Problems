6012bc127db69ab962582d44d2b74dbe5c41b8d0
Djaoued2006 <bhdmeddjaoued@gmail.com>
2023-10-29 13:59:42 +0100

    adding the leetcode151 to the leetcode repo

---
+++ leetcode151.py
@@ -0,0 +1,44 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        
+        s = s.strip() + " "
+
+        length = len(s)
+        i = 0
+
+        result = ""
+        word = ""
+        arr = []
+
+        while True:
+
+            if i == length: 
+                break
+            
+            if s[i] != " " :
+                word += s[i]
+            else :
+                if word != "":
+                    result = word +  " " + result
+                    word = ""
+            
+            i += 1
+            
+        return result[0 , len(result)-1]
+
+
+
+                
+            
+            
+            
+            
+
+
+            
+
+            
+print(Solution().reverseWords("hello     that is me"))
+print(len("hello that is me"))           
+                
+            
